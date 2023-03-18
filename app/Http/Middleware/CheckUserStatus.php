8b764e74cb1129f68efa39c6fa150889c3635497
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-03-18 15:44:15 -0500

    feat(middleware): add check user status middleware
    This commit adds a new middleware called "CheckUserStatus" that checks the status of the user and redirects to an error page if the user is disabled. This middleware can be used to restrict access to certain parts of the application based on the user's status.

---
+++ app/Http/Middleware/CheckUserStatus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserStatus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user()->status) {
+            return redirect()->route('home');
+        }
+
+        return $next($request);    }
+}
