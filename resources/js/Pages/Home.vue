0f1fc1c54bbe12c504edf9a9bc1084dbe2b57a10
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-04-01 14:57:45 -0500

    feat(home): replace Welcome and Dashboard views with Home view, update main Layout
    The Welcome.vue and Dashboard.vue views have been removed and replaced by the Home.vue view, which serves as the main landing page for the application. In addition, the main Layout has been updated to separate the navbar into its own component, improving code organization and reusability.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+});
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Home</h2>
+        </template>
+    </AuthenticatedLayout>
+</template>
