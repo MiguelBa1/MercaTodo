433a1b2bdb11659d42aa54d18bb57ae76b723bda
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-04-01 16:31:47 -0500

    feat(dashboard): add Dashboard view for site management
    The Dashboard.vue view has been added to provide the administrator with access to site management tools. The view is currently populated with the Card.vue component, which will be used to add additional functionality as it is developed.

---
+++ resources/js/Pages/Admin/Users/Partials/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <Link :href="href" class="bg-white overflow-hidden shadow rounded-lg">
+    <div class="p-6">
+      <div class="flex items-center mb-3">
+          <slot />
+        <h5 class="text-xl font-semibold pl-2">{{ title }}</h5>
+      </div>
+      <p class="text-gray-600">{{ subTitle }}</p>
+    </div>
+  </Link>
+</template>
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    subTitle: {
+        type: String,
+        required: true
+    },
+    href: {
+        type: String,
+        required: true
+    }
+})
+</script>
