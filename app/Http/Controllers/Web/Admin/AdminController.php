824341b88089f0bd6c9aafb3f9bcd939af07f0c7
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-04-02 17:34:59 -0500

    chore: reorganize routes and remove unused Vue component
    This commit moves the /admin route to a different location in the web.php file for improved organization. Additionally, a Vue component that was no longer being used has been removed from the project.

--- app/Http/Controllers/Web/Admin/AdminController.php
+++ app/Http/Controllers/Web/Admin/AdminController.php
@@ -10,7 +10,7 @@ class AdminController extends Controller
 {
     public function index(): Response
     {
-        return Inertia::render('Admin/Index');
+        return Inertia::render('Admin/Dashboard');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\Admin\RoleController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\Admin\UserController;
 use App\Http\Controllers\Web\Admin\AdminController;
 use App\Http\Controllers\Api\Admin\UserController as ApiUserController;
+use App\Http\Controllers\Api\Admin\RoleController as ApiRoleController;
 use App\Http\Controllers\Api\Admin\ProfileController as ApiProfileController;
 use \App\Http\Controllers\Api\Admin\PasswordController as ApiPasswordController;
 use Illuminate\Support\Facades\Route;
@@ -28,10 +28,6 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::get('/admin', function () {
-    return Inertia::render('Admin/Dashboard');
-})->middleware(['auth', 'role:admin', 'checkStatus', 'verified'])->name('admin.dashboard');
-
 Route::middleware(['auth', 'checkStatus', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -40,13 +36,14 @@ Route::middleware(['auth', 'checkStatus', 'verified'])->group(function () {
 
 // Render views
 Route::middleware(['auth', 'role:admin', 'checkStatus', 'verified'])->prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::get('users', [UserController::class, 'index'])->name('admin.view.users');
     Route::get('users/edit/{user}', [UserController::class, 'edit'])->name('admin.edit.user');
 });
 
 // API calls
 Route::middleware(['auth', 'role:admin', 'checkStatus', 'verified'])->prefix('admin/api')->group(function () {
-    Route::get('roles', [RoleController::class, 'list'])->name('admin.api.list.roles');
+    Route::get('roles', [ApiRoleController::class, 'list'])->name('admin.api.list.roles');
     Route::get('users', [ApiUserController::class, 'list'])->name('admin.api.list.users');
     Route::patch('users/status/{user}', [ApiUserController::class, 'update'])->name('admin.api.update.user.status');
     Route::patch('users/password/{user}', [ApiPasswordController::class, 'update'])->name('admin.api.update.user.password');
