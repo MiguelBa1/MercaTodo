2cee85307b27757c8a48d48fa549cc040d55640a
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-04-08 15:10:27 -0500

    feat: assign default role to user in RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,6 +44,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->assignRole('customer');
+
         event(new Registered($user));
 
         Auth::login($user);
