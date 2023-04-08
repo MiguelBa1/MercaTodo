3c148111ccb86408dd4b015ed3f2088ab39366eb
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-04-08 16:59:38 -0500

    feat: update RegistrationTest.php due to changes in RegisteredUserController.php
    Update the RegistrationTest.php file to reflect changes made in the RegisteredUserController.php file. This commit should have been made when the changes were made to the controller.

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Auth;
 
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
@@ -19,6 +20,8 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register(): void
     {
+        $role = new Role();
+        $role->create(['name' => 'customer']);
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
