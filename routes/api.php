971417c8d313fbfb4f009ce63b1f406cf55577b5
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-04-09 22:16:24 -0500

    feat(api-routes): move non-rendering routes to api.php
    Transferred routes that were not rendering a page from web.php to api.php, and uncommented "\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class" in Kernel.php to enable the
    middleware.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Admin\UserController as ApiUserController;
+use App\Http\Controllers\Api\Admin\RoleController as ApiRoleController;
+use App\Http\Controllers\Api\Admin\ProfileController as ApiProfileController;
+use \App\Http\Controllers\Api\Admin\PasswordController as ApiPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum', 'role:admin', 'checkStatus', 'verified'])->prefix('admin')->group(function () {
+    Route::get('roles', [ApiRoleController::class, 'list'])->name('admin.api.list.roles');
+    Route::get('users', [ApiUserController::class, 'list'])->name('admin.api.list.users');
+    Route::patch('users/status/{user}', [ApiUserController::class, 'update'])->name('admin.api.update.user.status');
+    Route::patch('users/password/{user}', [ApiPasswordController::class, 'update'])->name('admin.api.update.user.password');
+    Route::patch('users/profile/{user}', [ApiProfileController::class, 'update'])->name('admin.api.update.user.profile');
 });
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\Admin\UserController;
 use App\Http\Controllers\Web\Admin\AdminController;
-use App\Http\Controllers\Api\Admin\UserController as ApiUserController;
-use App\Http\Controllers\Api\Admin\RoleController as ApiRoleController;
-use App\Http\Controllers\Api\Admin\ProfileController as ApiProfileController;
-use \App\Http\Controllers\Api\Admin\PasswordController as ApiPasswordController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -34,20 +30,10 @@ Route::middleware(['auth', 'checkStatus', 'verified'])->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Render views
 Route::middleware(['auth', 'role:admin', 'checkStatus', 'verified'])->prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::get('users', [UserController::class, 'index'])->name('admin.view.users');
     Route::get('users/edit/{user}', [UserController::class, 'edit'])->name('admin.edit.user');
-});
-
-// API calls
-Route::middleware(['auth', 'role:admin', 'checkStatus', 'verified'])->prefix('admin/api')->group(function () {
-    Route::get('roles', [ApiRoleController::class, 'list'])->name('admin.api.list.roles');
-    Route::get('users', [ApiUserController::class, 'list'])->name('admin.api.list.users');
-    Route::patch('users/status/{user}', [ApiUserController::class, 'update'])->name('admin.api.update.user.status');
-    Route::patch('users/password/{user}', [ApiPasswordController::class, 'update'])->name('admin.api.update.user.password');
-    Route::patch('users/profile/{user}', [ApiProfileController::class, 'update'])->name('admin.api.update.user.profile');
 });
 
 require __DIR__ . '/auth.php';
