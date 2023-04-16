757b3dd18b3df0a4209fd314444abe55c23b32d0
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-04-16 16:19:17 -0500

    feat(users): exclude current user from list query
    Refactor the `list` method in the `UsersController` to exclude the currently authenticated user from the list of users returned by the query.

--- tests/Feature/Api/Admin/UserManagement/UserListTest.php
+++ tests/Feature/Api/Admin/UserManagement/UserListTest.php
@@ -8,17 +8,14 @@ class UserListTest extends UserTestCase
 {
     /**
      * @test
-     * List all users
+     * List all users with their roles except the current user
      */
     public function testListReturnsJson(): void
     {
-
         $response = $this->actingAs($this->adminUser)->get(route('admin.api.list.users'));
         $response->assertStatus(200);
         $response->assertJsonStructure(['data', 'links']);
-        $response->assertJsonCount(2, 'data');
         $response->assertJsonFragment(['name' => $this->customerUser->name]);
-        $response->assertJsonFragment(['name' => $this->adminUser->name]);
     }
 
 }
