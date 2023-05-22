08692d18fe15e57e9b557203eb78c330b8db7a82
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-05-21 22:39:29 -0500

    refactor: Rename controllers for better clarity
    The controllers ProductController and AdminController have been renamed to ProductDetailController and DashboardController, respectively. This change provides clearer and more descriptive names that better represent the specific functions and responsibilities of these controllers.

--- app/Http/Controllers/Web/Admin/DashboardController.php
+++ app/Http/Controllers/Web/Admin/DashboardController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class AdminController extends Controller
+class DashboardController extends Controller
 {
     public function index(): Response
     {
