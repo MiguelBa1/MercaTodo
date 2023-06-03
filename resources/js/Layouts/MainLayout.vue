c300926c3811cbedc73340e194f73702936a01ef
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-06-03 09:14:39 -0500

    refactor: Move syncCart call from app.js to MainLayout component
    This commit refactors the code by moving the `syncCart` function call from the `app.js` file to the `MainLayout` component. Previously, the `syncCart` API call was triggered on every page load, resulting in unnecessary network requests.

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,19 @@
 <script setup>
 import NavBar from "@/Components/NavBar.vue";
+import { useCartStore } from "@/store/cart";
+import { onMounted } from "vue";
+import {usePage} from "@inertiajs/vue3";
+
+const store = useCartStore();
+const { user } = usePage().props.auth;
+
+
+onMounted(async () => {
+    // if the user is logged in, fetch the cart from the server
+    if (user) {
+        await store.syncCart();
+    }
+});
 </script>
 
 <template>
