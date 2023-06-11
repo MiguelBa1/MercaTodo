89b99abb96254154cbdfd5dc5bcde4e9c7ee0561
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-06-11 00:09:31 -0500

    test(commands): add test for CheckPaymentSession command
    This commit adds a test for the console command 'app:check-payment-session' to ensure that it properly handles the payment status of pending orders.

---
+++ tests/Feature/Console/CheckPaymentSessionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Console;
+
+use App\Enums\OrderStatusEnum;
+use App\Models\Order;
+use Illuminate\Support\Facades\Http;
+use Tests\Feature\Utilities\UserTestCase;
+
+class CheckPaymentSessionTest extends UserTestCase
+{
+    public function testHandlePendingOrders(): void
+    {
+        /** @var Order[] $orders */
+        $orders = Order::factory()->count(3)->create([
+            'status' => OrderStatusEnum::PENDING,
+            'user_id' => $this->customerUser->id,
+        ]);
+
+        Http::fake([
+            config('placetopay.url') . 'api/session/*' => Http::response([
+                "status" => [
+                    "status" => "APPROVED",
+                    "reason" => "00",
+                    "message" => "La petición ha sido aprobada exitosamente",
+                    "date" => "2021-11-30T15:08:27-05:00",
+                ],
+            ])
+        ]);
+
+        $this->artisan('app:check-payment-session');
+
+        foreach ($orders as $order) {
+            $this->assertDatabaseHas('orders', [
+                'id' => $order->id,
+                'status' => OrderStatusEnum::COMPLETED,
+            ]);
+        }
+    }
+}
