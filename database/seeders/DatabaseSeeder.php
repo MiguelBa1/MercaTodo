1f7d87f9a2ac03e289791493338c3598c145e36e
Miguel Barrera <miguelbarreraa05@gmail.com>
2023-06-27 14:36:45 -0500

    feat: Add cache and public disk reset in DatabaseSeeder
    Restore cache and delete previously created images during database migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,32 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        /**
-         * Insert the cities, states and roles in the database
-         */
+        // Reset the cache
+        Cache::flush();
+
+        // Reset the storage
+        Storage::disk('public')->deleteDirectory('images');
+        Storage::disk('public')->makeDirectory('images');
+
         $this->call([
             DepartmentsCitiesSeeder::class,
             RolesSeeder::class,
+            UsersSeeder::class,
             BrandsSeeder::class,
             CategoriesSeeder::class,
             ProductsSeeder::class,
         ]);
-
-        User::factory(5)->create()->each(function ($user) {
-            $user->assignRole('customer');
-        });
-
-        // Create the admin user
-        User::factory()->create([
-            'name' => env('ADMIN_NAME'),
-            'surname' => env('ADMIN_SURNAME'),
-            'document_type' => env('ADMIN_DOCUMENT_TYPE'),
-            'document' => env('ADMIN_DOCUMENT'),
-            'email' => env('ADMIN_EMAIL'),
-            'phone' => env('ADMIN_PHONE'),
-            'address' => env('ADMIN_ADDRESS'),
-            'password' => bcrypt(env('ADMIN_PASSWORD')),
-            'city_id' => env('ADMIN_CITY_ID'),
-        ])->assignRole('admin');
     }
 }
